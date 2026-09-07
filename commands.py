ac065304c976ac40013991175edbd4a5576c644b
ezhang25 <evanzhang68@g.ucla.edu>
2026-09-07 18:37:37 -0400

    Added file tree visualizer

--- commands.py
+++ commands.py
@@ -6,3 +6,13 @@ def ls(directory: Directory=current_directory) -> list[Directory | File]:
     children = directory.get_children()
     for child in children:
         print(child.name)
+
+def tree(directory: Directory=current_directory, layer: int=1) -> str:
+    print(directory.name)
+    for child in directory.children:
+        for i in range(0, layer):
+            if i == layer-1:
+                print("  └──", end="")
+            else:
+                print("  │", end="  ")
+        tree(child, layer+1)
--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from files import *
 from commands import *
 
-root = Directory("random data", "/")
+root = Directory("random data", "/root")
 
 folder = Directory("random data", "folder")
 print(root.add_child(folder))
@@ -18,4 +18,11 @@ root.add_child(alphabetical)
 bottom = Directory("random data", "bottom")
 root.add_child(bottom)
 
+cow = Directory("random data", "cow")
+alphabetical.add_child(cow)
+
+bottom = Directory("random data", "bottom")
+alphabetical.add_child(bottom)
+
 ls(root)
+tree(root)
